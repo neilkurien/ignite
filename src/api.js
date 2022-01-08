4b3f43bcef3805962576f21b0483ab75ff45be71
Neil <nkurien97@gmail.com>
2022-01-09 01:54:11 +0530

    added api.js with popular games api req

---
+++ src/api.js
@@ -0,0 +1,36 @@
+//Base URL
+const base_url = "https://api.rawg.io/api/";
+const api_key_param = `&key=${process.env.REACT_APP_RAWG_API_KEY}`;
+
+//Getting the date
+const getCurrentMonth = () => {
+	const month = new Date().getMonth() + 1;
+	if (month < 10) {
+		return `0${month}`;
+	} else {
+		return month;
+	}
+};
+
+const getCurrentDay = () => {
+	const day = new Date().getDate();
+	if (day < 10) {
+		return `0${day}`;
+	} else {
+		return day;
+	}
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//Popular Games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `
+	${base_url}${popular_games}${api_key_param}
+`;
