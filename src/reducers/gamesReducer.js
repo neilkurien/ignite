3c3442ebeca4620afecc7389e77358ea5e82d5c5
Neil <nkurien97@gmail.com>
2022-01-09 17:27:17 +0530

    added new and upcoming games actions

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,13 +1,18 @@
 const initState = {
 	popular: [],
-	newGames: [],
 	upcoming: [],
+	newGames: [],
 };
 
 const gamesReducer = (state = initState, action) => {
 	switch (action.type) {
 		case "FETCH_GAMES":
-			return { ...state, popular: action.payload.popular };
+			return {
+				...state,
+				popular: action.payload.popular,
+				upcoming: action.payload.upcoming,
+				newGames: action.payload.new,
+			};
 		default:
 			return { ...state };
 	}
