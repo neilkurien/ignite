54f99b276d58ea27ac9923fb37ef0169b50cf535
Neil <nkurien97@gmail.com>
2022-01-09 23:44:05 +0530

    added back function from game detail and route tog

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,9 +10,10 @@ import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
 
 const Game = ({ name, released, image, id }) => {
-	//Load Details
+	//Load Detail
 	const dispatch = useDispatch();
 	const loadDetailHandler = () => {
+		document.body.style.overflow = "hidden";
 		dispatch(loadDetail(id));
 		console.log(id);
 	};
@@ -33,6 +34,7 @@ const StyledGame = styled(motion.div)`
 	text-align: center;
 	border-radius: 1rem;
 	cursor: pointer;
+	overflow: hidden;
 	img {
 		width: 100%;
 		height: 40vh;
--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,14 +6,24 @@ import { motion } from "framer-motion";
 
 //Redux
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const GameDetail = () => {
+	const navigate = useNavigate();
+	//Exit Detail
+	const exitDetailHandler = (e) => {
+		const element = e.target;
+		if (element.classList.contains("shadow")) {
+			document.body.style.overflow = "auto";
+			navigate("/");
+		}
+	};
 	//Data
 	const { screen, game, isLoading } = useSelector((state) => state.detail);
 	return (
 		<>
 			{!isLoading && (
-				<CardShadow>
+				<CardShadow className="shadow" onClick={exitDetailHandler}>
 					<Detail>
 						<Stats>
 							<div className="rating">
