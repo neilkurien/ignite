66a2ce2474f6e762229b8135e09413c9089c280c
Neil <nkurien97@gmail.com>
2022-01-11 01:01:11 +0530

    added platform icons and z-index fix

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { smallImage } from "../util";
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 //Styling and Animation
 import styled from "styled-components";
@@ -19,6 +25,25 @@ const GameDetail = ({ pathId }) => {
 			navigate("/");
 		}
 	};
+
+	//Get Platform Images
+	const getPlatform = (platform) => {
+		switch (platform) {
+			case "Playstation 4":
+				return playstation;
+			case "Xbox One":
+				return xbox;
+			case "PC":
+				return steam;
+			case "Nintendo Switch":
+				return nintendo;
+			case "iOS":
+				return apple;
+			default:
+				return gamepad;
+		}
+	};
+
 	//Data
 	const { screen, game, isLoading } = useSelector((state) => state.detail);
 	return (
@@ -37,9 +62,13 @@ const GameDetail = ({ pathId }) => {
 								<h3>Platforms</h3>
 								<Platforms>
 									{game.platforms.map((data) => (
-										<h3 key={data.platform.id}>
-											{data.platform.name}
-										</h3>
+										<img
+											src={getPlatform(
+												data.platform.name
+											)}
+											key={data.platform.id}
+											alt=""
+										></img>
 									))}
 								</Platforms>
 							</Info>
@@ -78,6 +107,7 @@ const CardShadow = styled(motion.div)`
 	position: fixed;
 	top: 0;
 	left: 0;
+	z-index: 5;
 	&::-webkit-scrollbar {
 		width: 0.5rem;
 	}
@@ -97,6 +127,7 @@ const Detail = styled(motion.div)`
 	position: absolute;
 	left: 10%;
 	color: black;
+	z-index: 10;
 	img {
 		width: 100%;
 	}
